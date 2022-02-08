cf929a9d97006090b39755c906a4f1fee082eb5b
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-02-09 02:02:17 +0600

    enable firebase And configaration firebase

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPBAlsgaCBVxXee7LbO3sqeeRmITqA1yg",
+    authDomain: "burj-al-arab-c296b.firebaseapp.com",
+    projectId: "burj-al-arab-c296b",
+    storageBucket: "burj-al-arab-c296b.appspot.com",
+    messagingSenderId: "1005486690748",
+    appId: "1:1005486690748:web:0b45a10dc1b97b1b4c9bb9"
+};
+
+export default firebaseConfig;
