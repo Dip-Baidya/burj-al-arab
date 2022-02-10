4b0c6b93f1250a526583869d541619a5ed7dbec9
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-02-10 20:50:29 +0600

    Ready Login And register Component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,25 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div className="login-form">
+            <div>
+                <h1>Log In Here PLease!</h1>
+                <form >
+                    <input type="email" name="" id="" placeholder="Your Email" />
+                    <br />
+                    <input type="password" name="" id="" />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>New to Burj Al Arab? <Link to ="/register">Create Account</Link> </p>
+                <div>------------or------------</div>
+                <button>Google Sign In</button>
+            </div>
+
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    return (
+        <div className="login-form">
+            <div>
+                <h2>Register: Create Account</h2>
+                <form onSubmit="">
+                    <input type="email" name="" id="" />
+                    <br />
+                    <input type="password" name="" id="" placeholder="your Password" />
+                    <br />
+                    <input type="password" name="" id="" placeholder="Re-enter Password" />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>Already have an Account? <Link to ="/login">log in</Link></p>
+                <div>------------or------------</div>
+                <button>Google Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
